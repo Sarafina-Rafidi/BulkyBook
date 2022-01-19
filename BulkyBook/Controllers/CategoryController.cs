4a7fa62a80f03916f92abea34522281c0b6b3f11
Sarafina-Rafidi <sarafina_rafidi@outlook.com>
2022-01-19 11:46:45 +0800

    Section 3 - Add Category View

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -18,5 +18,10 @@ namespace BulkyBook.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
